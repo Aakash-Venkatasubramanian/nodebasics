5bb5b47f36a5ba1882d274a18dc499f89dd543af
Aakash Venkatasubramanian <aakashvenkatasubramanian@gmail.com>
2020-03-31 13:14:52 +0530

    weather utils

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,37 +1,6 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
-
-// const weatherURL = 'https://api.darksky.net/forecast/c450c4ce68554b5df3581633fe44c4e9/37.8267,-122.4233?units=si'
-
-// request({url:weatherURL, json:true}, (error, response) => {
-//     if(error) {
-//         console.log('Unable to connect to internet')
-//     } else if(response.body.error) {
-//         console.log('Invalid coordinates')
-//     } else {
-//         const temperature = response.body.currently.temperature
-//         const precipitation = response.body.currently.precipProbability
-//         console.log('It is currently ' + temperature + ' degrees out. There is a ' + precipitation + '% chance of rain.')
-//     }
-// })
-
-const weather = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/c450c4ce68554b5df3581633fe44c4e9/' + latitude + ',' + longitude + '?units=si'
-    request({url:url, json:true}, (error, response) => {
-        if(error) {
-            callback('Unable to connect to internet', undefined)
-        } else if(response.body.error) {
-            callback('Invalid coordinates', undefined)
-        } else {
-            const temperature = response.body.currently.temperature
-            const precipitation = response.body.currently.precipProbability
-            callback(undefined, {
-                temperature:temperature,
-                precipitation:precipitation
-            })
-        }
-    })
-}
+const weather = require('./utils/weather')
 
 weather(13.09, 80.27, (error, data) => {
     if(data === undefined) {
